1f2155c8cf7f5bf7a51e35c535063e48f10f1878
danilkk <ezzfvkoko@gmail.com>
2023-09-19 00:42:54 +0300

    feat: change slider to buttonGroup + add outlined if frequency chosen

--- src/page/ChartPage.tsx
+++ src/page/ChartPage.tsx
@@ -6,7 +6,6 @@ import FFTChart from '../widgets/fftChart';
 import styles from "./ChartPage.module.scss";
 import Button from '@mui/material/Button';
 import ButtonGroup from '@mui/material/ButtonGroup';
-import Slider from '@mui/material/Slider';
 import { useDebounce } from "use-debounce";
 
 const ChartPage = () => {
@@ -14,39 +13,37 @@ const ChartPage = () => {
     const [frequency, setFrequency] = useState<number>(8);
     const pointMarks = [
         {
-            value: 10,
-            label: '10 Гц',
+            value: 63,
+            label: '64 Гц',
         },
         {
-            value: 200,
-            label: '200 Гц',
+            value: 127,
+            label: '128 Гц',
         },
         {
-            value: 500,
-            label: '500 Гц',
+            value: 255,
+            label: '256 Гц',
         },
         {
-            value: 1000,
-            label: '1 КГц',
+            value: 511,
+            label: '512 Гц',
         },
         {
-            value: 2000,
-            label: '2 КГц',
+            value: 1023,
+            label: '1024 Гц',
         },
         {
-            value: 5000,
-            label: '5 КГц',
+            value: 2047,
+            label: '2048 Гц',
+        },
+        {
+            value: 4095,
+            label: '4096 Гц',
         },
     ]
 
-    const [points, setPoints] = useState(500);
+    const [points, setPoints] = useState(1023);
     const [debouncedPoints] = useDebounce(points, 500)
-
-    const handleSliderPointsChange = (event: Event) => {    
-        const target = event.target as HTMLInputElement;
-        const value = Number(target.value);
-        setPoints(value);
-      };
   
     useEffect(() => {
         const harmonic = generateHarmonicSignal(frequency, debouncedPoints);
@@ -67,25 +64,19 @@ const ChartPage = () => {
                 <h2>Панель управления</h2>
                     <h3>Частота сигнала:</h3>
                     <ButtonGroup variant="contained" aria-label="outlined button group">
-                        <Button onClick={() => setFrequency(1)}>1 Гц</Button>
-                        <Button onClick={() => setFrequency(2)}>2 Гц</Button>
-                        <Button onClick={() => setFrequency(4)}>4 Гц</Button>
-                        <Button onClick={() => setFrequency(8)}>8 Гц</Button>
+                        <Button variant={frequency !== 1 ? "contained" : "outlined"} onClick={() => setFrequency(1)}>1 Гц</Button>
+                        <Button variant={frequency !== 2 ? "contained" : "outlined"} onClick={() => setFrequency(2)}>2 Гц</Button>
+                        <Button variant={frequency !== 4 ? "contained" : "outlined"} onClick={() => setFrequency(4)}>4 Гц</Button>
+                        <Button variant={frequency !== 8 ? "contained" : "outlined"} onClick={() => setFrequency(8)}>8 Гц</Button>
                     </ButtonGroup>
                     
                     <div>
                         <h3>Частота дискретизации (количество точек в массиве сигнала):</h3>
-                        <Slider
-                            className={styles.slider}
-                            aria-label="Always visible"
-                            defaultValue={500}
-                            max={5000}
-                            min={10}
-                            onChange={handleSliderPointsChange}
-                            step={10}
-                            marks={pointMarks}
-                            valueLabelDisplay="auto"
-                        />
+                        <ButtonGroup variant="contained" aria-label="outlined button group">
+                            {pointMarks.map(val => (
+                                <Button key={val.value} variant={val.value !== points ? "contained" : "outlined"} onClick={() => setPoints(val.value)}>{val.label}</Button>
+                            ))}
+                        </ButtonGroup>
                     </div>
             </aside>
             <div className={styles.chartPanel}>
